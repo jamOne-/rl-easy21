b3cd0cd6ae162737ff527b75f74dc1057caff2a8
jamone- <dominik.rabij@gmail.com>
2018-10-16 21:53:34 +0200

    easy21: step returns is_terminal

--- easy21.py
+++ easy21.py
@@ -18,25 +18,35 @@ def init_state():
 def step(state, action):
   dealer, player = state
   reward = 0
+  terminal = False
 
-  if action == 'hit':
+  if action == 'h' or action == 'hit':
     color, value = draw_card()
     player += color * value
-    reward = -1 if player < 1 or player > MAX_VALUE else 0
+    player_busted = is_busted(player)
 
-  elif action == 'stick':
+    reward = -1 if player_busted else 0
+    terminal = player_busted
+
+  elif action == 's' or action == 'stick':
     while dealer > 0 and dealer < DEALER_STICK_AT:
       color, value = draw_card()
       dealer += color * value
-      reward = calculate_reward(dealer, player)
 
-  return reward, (dealer, player)
+    reward = calculate_reward(dealer, player)
+    terminal = True
+
+  return reward, (dealer, player), terminal
 
 
 def calculate_reward(dealer, player):
-  if dealer > MAX_VALUE or player > dealer:
+  if is_busted(dealer) or player > dealer:
       return 1
   elif player == dealer:
     return 0
   else:
     return -1
+
+
+def is_busted(player):
+  return player < 1 or player > MAX_VALUE
